a2861cc47aec066a74549e2f5d57b4686887c9fa
wilas0o0o <03mcu30@gmail.com>
2021-11-22 05:46:36 +0000

    [Create]Create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,21 @@ class BooksController < ApplicationController
     @book=Book.new
     @books=Book.all
   end
-
+  
+  def create
+    @book=Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
